078e76e0371179ddf50ff4984ce8aa122db8d896
Azat Akhunov <azat.akhunov@protonmail.com>
2021-06-12 16:39:14 +0300

    game window creation added

--- src/main/java/ru/mycubecraft/Game.java
+++ src/main/java/ru/mycubecraft/Game.java
@@ -1,8 +1,11 @@
 package ru.mycubecraft;
 
+import ru.mycubecraft.window.Window;
+
 public class Game {
 
     public static void main(String[] args) {
-
+        Window gameWindow = Window.getInstance();
+        gameWindow.run();
     }
 }
---
+++ src/main/java/ru/mycubecraft/window/Window.java
@@ -0,0 +1,87 @@
+package ru.mycubecraft.window;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window {
+
+    private int width, height;
+    private String title;
+    private  long glfwWindow;
+
+    private static Window window = null;
+
+    private Window() {
+        this.width = 800;
+        this.height = 600;
+        this.title = "CubeCraft Game";
+    }
+
+    public static Window getInstance() {
+        if (window == null) {
+            window = new Window();
+        }
+        return window;
+    }
+
+    public void run() {
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+
+        init();
+        loop();
+    }
+
+
+    public void init() {
+        // Setup an error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW.");
+        }
+
+        // Configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
+
+        // Create the window where glfwWindow stores the memory address of created window
+        this.glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if (this.glfwWindow == NULL) {
+            throw new IllegalStateException("Failed to create the GLFW window.");
+        }
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(this.glfwWindow);
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        GL.createCapabilities();
+    }
+
+
+    public void loop() {
+        while(!glfwWindowShouldClose(this.glfwWindow)){
+
+            // Poll events
+            glfwPollEvents();
+
+            glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwSwapBuffers(this.glfwWindow);
+        }
+    }
+
+
+
+
+
+
+}
