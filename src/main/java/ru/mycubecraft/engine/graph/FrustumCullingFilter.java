34548bcfb5267b5b9cc99e4ec72aec04c87a05e2
Azat Akhunov <azat.akhunov@protonmail.com>
2021-08-19 20:58:56 +0300

    rendering speed increased via frustum and decreasing elements for a render

--- src/main/java/ru/mycubecraft/engine/graph/FrustumCullingFilter.java
+++ src/main/java/ru/mycubecraft/engine/graph/FrustumCullingFilter.java
@@ -2,7 +2,10 @@ package ru.mycubecraft.engine.graph;
 
 import org.joml.FrustumIntersection;
 import org.joml.Matrix4f;
+import org.joml.Vector2f;
 import org.joml.Vector3f;
+import org.joml.primitives.Intersectionf;
+import ru.mycubecraft.Settings;
 import ru.mycubecraft.core.GameItem;
 import ru.mycubecraft.renderer.Camera;
 
@@ -11,12 +14,14 @@ import java.util.ArrayList;
 public class FrustumCullingFilter {
 
     private final Matrix4f prjViewMatrix;
-
+    private final Vector2f nearFar;
+    private float closestDistance = Settings.Z_FAR;
     private final FrustumIntersection frustumInt;
 
     public FrustumCullingFilter() {
         prjViewMatrix = new Matrix4f();
         frustumInt = new FrustumIntersection();
+        nearFar = new Vector2f();
     }
 
     public void updateFrustum(Matrix4f projMatrix, Matrix4f viewMatrix) {
@@ -38,7 +43,20 @@ public class FrustumCullingFilter {
             boundingRadius = gameItem.getScale() * gameItem.getMesh().getBoundingRadius();
             gameItemPosition = gameItem.getPosition();
             boolean insideTheFrustum = insideFrustum(gameItemPosition.x, gameItemPosition.y, gameItemPosition.z, boundingRadius);
-            gameItem.setInsideFrustum(insideTheFrustum);
+            Vector3f min = new Vector3f();
+            Vector3f max = new Vector3f();
+            min.set(gameItem.getPosition());
+            max.set(gameItem.getPosition());
+            min.add(-10, -10, -10);
+            max.add(10, 10, 10);
+            float xStart = camera.getPosition().x;
+            float yStart = camera.getPosition().y;
+            float zStart = camera.getPosition().z;
+
+            Vector3f origin = new Vector3f(xStart, yStart, zStart);
+            if (Intersectionf.intersectRayAab(origin, origin.normalize(), min, max, nearFar) && nearFar.x < closestDistance) {
+                gameItem.setInsideFrustum(insideTheFrustum);
+            }
         }
     }
 
