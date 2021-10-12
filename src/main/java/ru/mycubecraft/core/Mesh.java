9c1647fcf2744c51f763cafedd6e2dcaaf4d295a
Azat Akhunov <azat.akhunov@protonmail.com>
2021-10-12 16:38:59 +0300

    shader program var locations getter updated

--- src/main/java/ru/mycubecraft/core/Mesh.java
+++ src/main/java/ru/mycubecraft/core/Mesh.java
@@ -69,20 +69,16 @@ public class Mesh {
             GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboId);
             GL15.glBufferData(GL15.GL_ARRAY_BUFFER, textCoordsBuffer, GL15.GL_STREAM_DRAW);
             GL20.glVertexAttribPointer(1, 2, GL11.GL_FLOAT, false, 0, 0);
-//
-//            // Vertex normals VBO
-//            vboId = GL15.glGenBuffers();
-//            vboIdList.add(vboId);
-//            vecNormalsBuffer = MemoryUtil.memAllocFloat(normals.length);
-//            if (vecNormalsBuffer.capacity() > 0) {
-//                vecNormalsBuffer.put(normals).flip();
-//            } else {
-//                vecNormalsBuffer = MemoryUtil.memAllocFloat(positions.length);
-//                vecNormalsBuffer.put(positions).flip();
-//            }
-//            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboId);
-//            GL15.glBufferData(GL15.GL_ARRAY_BUFFER, vecNormalsBuffer, GL15.GL_STREAM_DRAW);
-//            GL20.glVertexAttribPointer(2, 3, GL11.GL_FLOAT, false, 0, 0);
+
+            // Vertex normals VBO
+            vboId = GL15.glGenBuffers();
+            vboIdList.add(vboId);
+            vecNormalsBuffer = MemoryUtil.memAllocFloat(normals.length);
+            vecNormalsBuffer.put(normals).flip();
+
+            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboId);
+            GL15.glBufferData(GL15.GL_ARRAY_BUFFER, vecNormalsBuffer, GL15.GL_STATIC_DRAW);
+            GL20.glVertexAttribPointer(2, 3, GL11.GL_FLOAT, false, 0, 0);
 
             // Index VBO
             vboId = GL15.glGenBuffers();
@@ -121,39 +117,6 @@ public class Mesh {
         return vertexCount;
     }
 
-//    public void renderList(List<GameItem> gameItems, Consumer<GameItem> consumer) {
-//        initRender();
-//
-//        for (GameItem gameItem : gameItems) {
-//            // Set up data required by GameItem
-//            consumer.accept(gameItem);
-//            // Render this game item
-//            glDrawElements(GL_TRIANGLES, vertexCount, GL_UNSIGNED_INT, 0);
-//        }
-//
-//        endRender();
-//    }
-
-//    private void initRender() {
-//        Texture texture = material.getTexture();
-//        if (texture != null) {
-//            // Activate firs texture bank
-//            glActiveTexture(GL_TEXTURE0);
-//            // Bind the texture
-//            glBindTexture(GL_TEXTURE_2D, texture.getId());
-//        }
-//
-//        // Draw the mesh
-//        glBindVertexArray(vaoId);
-//    }
-
-//    private void endRender() {
-//        // Restore state
-//        glBindVertexArray(0);
-//
-//        glBindTexture(GL_TEXTURE_2D, 0);
-//    }
-
     public void render() {
         Texture texture = material.getTexture();
         if (texture != null) {
@@ -162,20 +125,19 @@ public class Mesh {
             // Bind the texture
             glBindTexture(GL_TEXTURE_2D, texture.getId());
         }
-//        Texture normalMap = material.getNormalMap();
-//        if (normalMap != null) {
-//            // Activate first texture bank
-//            glActiveTexture(GL_TEXTURE1);
-//            // Bind the texture
-//            glBindTexture(GL_TEXTURE_2D, normalMap.getId());
-//        }
+        Texture normalMap = material.getNormalMap();
+        if (normalMap != null) {
+            // Activate first texture bank
+            glActiveTexture(GL_TEXTURE1);
+            // Bind the texture
+            glBindTexture(GL_TEXTURE_2D, normalMap.getId());
+        }
 
         // Draw the mesh
         GL30.glBindVertexArray(vaoId);
-
         GL20.glEnableVertexAttribArray(0);
         GL20.glEnableVertexAttribArray(1);
-//        GL20.glEnableVertexAttribArray(2);
+        GL20.glEnableVertexAttribArray(2);
 
         glDrawElements(GL_TRIANGLES, vertexCount, GL_UNSIGNED_INT, 0);
 
@@ -194,9 +156,10 @@ public class Mesh {
 
 
     public void cleanUp() {
+        // Restore state
         GL20.glDisableVertexAttribArray(0);
         GL20.glDisableVertexAttribArray(1);
-//        GL20.glDisableVertexAttribArray(2);
+        GL20.glDisableVertexAttribArray(2);
 
         // Delete the VBOs
         glBindBuffer(GL_ARRAY_BUFFER, 0);
